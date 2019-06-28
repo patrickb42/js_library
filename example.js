7a51296ef65e2522c4d5ce68595ce9644cb57ae4
patrickb42 <51134285+patrickb42@users.noreply.github.com>
2019-06-28 17:00:15 -0400

    Update example.js

--- example.js
+++ example.js
@@ -1,45 +1,50 @@
-function rateLimitedFunctionFactory({ sideEffect, timeout = 300 }) {
-  let locked = false;
-  let triggeredWhileLocked = false;
+(() => {
+  function rateLimitedFunctionFactory({ sideEffect, timeout = 300, triggeredWhileLockedEffect }) {
+    let locked = false;
+    let triggeredWhileLocked = false;
 
-  return () => {
-    if (!locked) {
-      locked = true;
-      sideEffect();
-      setTimeout(() => {
-        locked = false;
-        if (triggeredWhileLocked) {
-          triggeredWhileLocked = false;
-          sideEffect();
-        }
-      }, timeout);
-    } else {
-      triggeredWhileLocked = true;
-    }
-  };
-}
-
-// class-free menu button constructor function
-function newMenuButton({ menuBtnElement }) {
-  const _nav = document.querySelector('nav');
-  const _maxMobileWidth = 500;
-
-  const toggleNav = () => {
-    _nav.classList.toggle('menu--open');
-  };
-
-  const conditionallyCloseMenu = rateLimitedFunctionFactory({
-    sideEffect: () => {
-      if (parseInt(window.innerWidth, 10) > _maxMobileWidth) {
-        _nav.classList.remove('menu--open');
+    return () => {
+      if (!locked) {
+        locked = true;
+        sideEffect();
+        setTimeout(() => {
+          locked = false;
+          if (triggeredWhileLocked) {
+            triggeredWhileLocked = false;
+            sideEffect();
+          }
+        }, timeout);
+      } else {
+        triggeredWhileLocked = true;
+        if (triggeredWhileLockedEffect) triggeredWhileLockedEffect();
       }
-    },
-    // timeout: 250, // this line is where you can optionally change the default timeout duration
-  });
+    };
+  }
 
-  menuBtnElement.addEventListener('click', toggleNav);
-  window.addEventListener('resize', conditionallyCloseMenu);
-}
+  function newMenuButton({ menuBtnElement }) {
+    const _nav = document.querySelector('nav');
+    const _maxMobileWidth = 500;
+
+    const toggleNav = () => {
+      _nav.classList.toggle('menu--open');
+    };
+
+    const conditionallyCloseMenu = rateLimitedFunctionFactory({
+      sideEffect: () => {
+        if (parseInt(window.innerWidth, 10) > _maxMobileWidth) {
+          _nav.classList.remove('menu--open');
+        }
+      },
+      // optionally change the default timeout duration
+      // timeout: 250,
+      // optionally add a function that runs when the event is triggered but locked
+      // lockoutEffect: () => alert('locked out'),
+    });
+
+    menuBtnElement.addEventListener('click', toggleNav);
+    window.addEventListener('resize', conditionallyCloseMenu);
+  }
 
 
-newMenuButton({ menuBtnElement: document.querySelector('.menu-btn') });
+  newMenuButton({ menuBtnElement: document.querySelector('.menu-btn') });
+})();
