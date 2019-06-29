42d5807a6ea49d271ab052b4cecaad3d1209670e
patrickb42 <51134285+patrickb42@users.noreply.github.com>
2019-06-28 20:53:32 -0400

    added annotations

--- rateLimitedFunctionFactory.js
+++ rateLimitedFunctionFactory.js
@@ -1,3 +1,9 @@
+// This a function that can take a function, like one that you would apply add to an event listener, and return a function that wraps your logic in a rate-limiter.
+// It has a default value of 300ms, but you can change how often it fires the side effect.
+// It also makes it so the function will run if the event was triggered when the function was in a locked state.
+// On top of all of that, because the actual function being added to the event listener is a stored value, and not an IIFE, you are able to remove the event listener later.
+// This has the ability to add a function that runs if the event is triggered while it is locked out, such as notifying the user that there's a cool-down on a button.
+
 (() => {
   function rateLimitedFunctionFactory({ sideEffect, triggeredWhileLockedEffect, timeout = 300 }) {
     let locked = false;
