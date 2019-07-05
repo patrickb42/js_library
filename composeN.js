2b11e84e235ca17e4699647aaeff860f3389aa39
patrickb42 <51134285+patrickb42@users.noreply.github.com>
2019-07-05 18:11:39 -0400

    Create composeN.js

---
+++ composeN.js
@@ -0,0 +1,29 @@
+/*
+ * this function is designed to take any number of functions and call them from right
+ * to left; this curries the data. The return from the far right will be the argument
+ * off the function that is to its left.
+ */
+(() => {
+  function composeN(...fns) {
+    return fns.reduceRight((acc, curr) => {
+      return (value) => {
+        return curr(acc(value));
+      };
+    });
+  }
+
+  function adderFactory(adder) {
+    return subject => subject + adder;
+  }
+
+  function multiplierFactory(multiplier) {
+    return subject => subject * multiplier;
+  }
+
+  const composedFunction = composeN(adderFactory(1),
+    multiplierFactory(2),
+    adderFactory(-3));
+
+  // the same as doing adderFactory(1)(multiplierFactory(2)(adderFactory(-3)(100)));
+  console.log(composedFunction(100));
+})();
